cb14654832845f3bc72b5640d6e9842b3d4bf7ff
ProstoRocker <SaberSai608@gmail.com>
2026-08-05 14:46:08 +0500

    Test: fix Robolectric and Espresso setup

--- app/src/test/java/com/ilyadev/meowmoments/data/repository/CatFactsRepositoryIntegrationTest.kt
+++ app/src/test/java/com/ilyadev/meowmoments/data/repository/CatFactsRepositoryIntegrationTest.kt
@@ -17,6 +17,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
 
+
 /** Integration Tests (Интеграционные тесты)
 Цель:
 - Проверить взаимодействие между слоями
